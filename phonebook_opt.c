accd1ebaf46876ed278217d5c64fd7d53d12aae6
ssh <magan0117@hotmail.com>
2015-10-26 15:39:25 +0800

    Hw2 smaller structere ver2
    fix bug about findName

--- phonebook_opt.c
+++ phonebook_opt.c
@@ -1,5 +1,5 @@
 #include <stdlib.h>
-#include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
 #include <ctype.h>
 
@@ -8,48 +8,29 @@
 
 /* FILL YOUR OWN IMPLEMENTATION HERE! */
 
-
 /* original version */
-entry *findName(char lastname[], entry *pHead)
+entry *findName(char lastName[], entry *pHead)
 {
     while (pHead != NULL) {
-        if (strcasecmp(lastname, pHead->lastName) == 0) {
+        if (strcasecmp(lastName, pHead->lastName) == 0) {
+            printf(" %12s  is found!\n", lastName);
             return pHead;
         }
         pHead = pHead->pNext;
     }
+    printf(" %12s  is not found!\n", lastName);
     return NULL;
 }
 
 entry *append(char lastName[], entry *e)
 {
-    /* allocate memory for the new entry and put lastName */
+    /* allocate memory for the new entry and put lastName in it.*/
     e->pNext = (entry *) malloc(sizeof(entry));
     e = e->pNext;
     strcpy(e->lastName, lastName);
     e->pNext = NULL;
+
     return e;
 }
 
-//newww
-
-lastNameEntry *OptfindName(char lastName[], lastNameEntry *pHead)
-{
-    while (pHead != NULL) {
-        if (strcasecmp(lastName, pHead->lastName) == 0) {
-            return pHead;
-        }
-        pHead = pHead->pNext;
-    }
-    return NULL;
-}
-lastNameEntry *Optappend(char lastName[], lastNameEntry *lne)
-{
-    /* allocate memory for the new entry and put lastName in it.*/
-    lne->pNext = (lastNameEntry *) malloc(sizeof(lastNameEntry));
-    lne = lne->pNext;
-    strcpy(lne->lastName, lastName);
-    lne->pNext = NULL;
-    return lne;
-}
 
--- phonebook_opt.h
+++ phonebook_opt.h
@@ -2,9 +2,8 @@
 #define _PHONEBOOK_H
 #define MAX_LAST_NAME_SIZE 16
 
-/* original version */
-
-typedef struct __PHONE_BOOK_ENTRY {
+//detail part
+typedef struct __PHONE_BOOK_DETAIL {
     char lastName[MAX_LAST_NAME_SIZE];
     char firstName[16];
     char email[16];
@@ -15,20 +14,16 @@ typedef struct __PHONE_BOOK_ENTRY {
     char city[16];
     char state[2];
     char zip[5];
+    struct __PHONE_BOOK_DETAIL *pNext;
+} detail;
+
+//struct lastname part
+typedef struct __PHONE_BOOK_ENTRY {
+    char lastName[MAX_LAST_NAME_SIZE];
+    detail *detail;
     struct __PHONE_BOOK_ENTRY *pNext;
 } entry;
 
 entry *findName(char lastname[], entry *pHead);
 entry *append(char lastName[], entry *e);
-
-typedef struct __LAST_NAME_ENTRY {
-    char lastName[MAX_LAST_NAME_SIZE];
-    entry *detail;
-    struct __LAST_NAME__ENTRY *pNext;
-} lastNameEntry;
-lastNameEntry *OptfindName(char lastname[], lastNameEntry *pHead);
-lastNameEntry *Optappend(char lastName[], lastNameEntry *lne);
-
-
-
 #endif
